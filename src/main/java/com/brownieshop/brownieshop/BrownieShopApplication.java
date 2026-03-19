db8686537613e364934f081d152bbdf60c6f87a8
Mathish Sasmitha <IT24101669@my.sliit.lk>
2026-03-19 13:00:49 +0530

    appliction propertie file

---
+++ src/main/java/com/brownieshop/brownieshop/BrownieShopApplication.java
@@ -0,0 +1,12 @@
+package com.brownieshop.brownieshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrownieShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrownieShopApplication.class, args);
+    }
+}
